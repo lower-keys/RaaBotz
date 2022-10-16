e9db3655efcdbded921ae6cdbe10fc6caa8bb750
Andromeda <lowerkeys9@gmail.com>
2022-10-16 07:59:03 +0700

    Aaaa

--- plugins/menu.js
+++ plugins/menu.js
@@ -333,9 +333,9 @@ ${'```%npmdesc```'}
        } 
      }) 
      if (teks == '404') {
- 	let cute = `${pickRandom(['https://telegra.ph/file/520a7d8d531f976ed23b5.jpg', 'https://telegra.ph/file/862e5a42d3435e12b2ec2.jpg', 'https://telegra.ph/file/aebb49a158df5a6da47d6.jpg', 'https://telegra.ph/file/8d0f6476b51d76eaf8cea.jpg','https://telegra.ph/file/cc86d58dd6c6ec6828ce0.jpg','https://telegra.ph/file/309fe309cf1a8f0b9a463.jpg', 'https://telegra.ph/file/10377f92ab3043979485b.jpg', 'https://telegra.ph/file/b4a60b95b203cfb6678f3.jpg', 'https://telegra.ph/file/81ac8ecd55d4a6deb8463.jpg'])}` 
+ 	let cute = `${pickRandom(['https://i.postimg.cc/8zCXVkyY/ce6ffeea35742410166301aede74af0d.jpg', 'https://i.postimg.cc/g01ZNptQ/2581c5803ef77dfad121830c07873129.jpg', 'https://i.postimg.cc/GmQBvddb/3db17e4c76671353a6896ff4ad6b5eb8.jpg'])}` 
  	let ori = `Hi , @${m.sender.split`@`[0]}
-Kenalin gwe bilek😅 
+*Andromeda Here 🙌*
 `
 /*conn.sendMessage(m.chat, {
     	react: {
@@ -343,7 +343,7 @@ Kenalin gwe bilek😅
     		key: m.key
     	}
     })*/
-return conn.send3ButtonLoc(m.chat, await conn.resize(pickRandom(global.waifu), 300, 300), ori, `Kalo Ga Support Button Klik ${_p}simplemenu`, 'COMMAND', '.simplemenu', 'DONATION', '.donasi', 'RENT BOT', '.sewa', m)
+return conn.send3ButtonLoc(m.chat, await conn.resize(pickRandom(global.takina), 300, 300), ori, `Kalo Ga Support Button Klik ${_p}simplemenu`, 'COMMAND', '.simplemenu', 'DONATION', '.donasi', 'RENT BOT', '.sewa', m)
     
     }
   
